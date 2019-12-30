9449afe83a4dd7b4be11aa4c169e6ce78bd4ac25
venom <biraj.silwalhmb2@gmail.com>
2019-12-29 19:01:10 -0700

    started working on lobotask its gonna be something different :D

---
+++ app/src/main/java/com/example/mylobo/Lobotask/Lobotask.java
@@ -0,0 +1,16 @@
+package com.example.mylobo.Lobotask;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mylobo.R;
+
+public class Lobotask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView( R.layout.activity_lobotask);
+    }
+}
